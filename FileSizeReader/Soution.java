d539ac455025367cc03cbb3ee5f303fe4d30da18
Moss89 <35338105+Moss89@users.noreply.github.com>
2022-03-11 16:32:32 +0000

    Update Soution.java

--- FileSizeReader/Soution.java
+++ FileSizeReader/Soution.java
@@ -5,32 +5,22 @@ class Solution {
     }
 
     static public String P1(String S) {
-//        split the string with space
         String[] inputLines = S.split("\n");
-//       create the input string
         String myString = "";
-//        initialize count these four type
         int image = 0;
         int movie = 0;
         int music = 0;
         int other = 0;
 
-//        create a string for the output
         String result = "";
-//        for loop each string that have split by space
         for (int i = 0; i<inputLines.length; i++){
             myString = inputLines[i];
-//            split each line's string by the last occurrence of "." and the space " " , return the file's type
             int extensionDot = myString.lastIndexOf( '.');
             int space = myString.indexOf(' ');
-//            extensionType is the string of the file's type
             String extensionType = myString.substring(extensionDot+1,space);
-//            split each line's string by last occurrence of "b" and the space" ", return the string size
             int bits = myString.lastIndexOf('b');
             String size = myString.substring(space+1,bits);
-//            turn the string size to int
             int sizeInt = Integer.parseInt(size);
-//          this is different cases if the type is music/image/movie type, add their size together
             switch(extensionType){
                 case "mp3":
                 case "aac":
@@ -47,16 +37,12 @@ class Solution {
                 case "mkv":
                     movie += sizeInt;
                     break;
-
-//        considering the there have other kind of file may be occured in this string
-//        I added the others type of file's size together, print them out as well
                 default:
                     other += sizeInt;
 
             }
 
         }
-//        print the result
         result = "music " + music + "b\nimages " + image + "b\nmovies " + movie + "b\nother " + other + "b";
         return result;
     }
